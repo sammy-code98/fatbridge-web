09e570ffd8f0334e90268b14a7169b40c4033e0b
sammy-code98 <sammysmart98@yahoo.com>
2022-02-11 21:48:56 +0100

    feat:add icons

--- components/Vision/Vision.jsx
+++ components/Vision/Vision.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import {
-  FaFacebook,
-  FaTwitter,
-  FaLinkedin,
-  FaGooglePlusG,
+  FaGraduationCap,
+  FaBookOpen,
+  FaFlask,
+  FaCalculator,
 } from "react-icons/fa";
 
 function Vision() {
   return (
     <div className="md:grid  md:grid-cols-4 md:gap-6 md:justify-items-center p-4 md:px-24  mt-24 sm:grid sm:grid-cols-2 sm:gap-8">
       <div className="bg-purple-200 w-full rounded-md shadow-md mt-24">
-        <div className="flex mx-auto -translate-y-12 rounded-full text-center shadow-2xl h-20 w-20 text-gray-700  bg-white">
-        <FaFacebook
-          className="cursor-pointer  text-gray-500 "
-          style={{ fontSize: "1.5em" }}
-        />        </div>
+        <div className="flex mx-auto -translate-y-12 rounded-full  shadow-2xl h-20 w-20 text-gray-700  bg-white">
+          <FaGraduationCap
+            className="flex mx-auto mt-4  text-gray-500 "
+            style={{ fontSize: "2em" }}
+          />{" "}
+        </div>
         <div className="text-gray-500 text-left text-base  mx-3">
           lorem dorem
         </div>
@@ -25,7 +26,10 @@ function Vision() {
       {/* 2nd */}
       <div className="bg-amber-200 w-full rounded-md shadow-md mt-24">
         <div className="flex mx-auto -translate-y-12 rounded-full text-center shadow-2xl h-20 w-20 text-gray-700  bg-white">
-          {/* {vision.icon} */}
+          <FaBookOpen
+            className="flex mx-auto mt-4  text-gray-500 "
+            style={{ fontSize: "2em" }}
+          />
         </div>
         <div className="text-gray-500 text-left text-base  mx-3">
           lorem dorem
@@ -37,7 +41,10 @@ function Vision() {
       {/* 3rd */}
       <div className="bg-green-200 w-full rounded-md shadow-md mt-24">
         <div className="flex mx-auto -translate-y-12 rounded-full text-center shadow-2xl h-20 w-20 text-gray-700  bg-white">
-          {/* {vision.icon} */}
+        <FaFlask
+            className="flex mx-auto mt-4  text-gray-500 "
+            style={{ fontSize: "2em" }}
+          />
         </div>
         <div className="text-gray-500 text-left text-base  mx-3">
           lorem dorem
@@ -49,7 +56,10 @@ function Vision() {
       {/* 4th */}
       <div className="bg-yellow-200 w-full rounded-md shadow-md mt-24">
         <div className="flex mx-auto -translate-y-12 rounded-full text-center shadow-2xl h-20 w-20 text-gray-700  bg-white">
-          {/* {vision.icon} */}
+        <FaCalculator
+            className="flex mx-auto mt-4  text-gray-500 "
+            style={{ fontSize: "2em" }}
+          />
         </div>
         <div className="text-gray-500 text-left text-base  mx-3">
           lorem dorem
