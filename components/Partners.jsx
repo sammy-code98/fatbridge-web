7d6af8e0f7b3c2e879c1542fe37a59e7850e3743
sammy-code98 <sammysmart98@yahoo.com>
2022-02-16 21:14:11 +0100

    feat: add brand logo

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import Ticker from "react-ticker";
+import Image from "next/image";
 
 function Partners() {
   return (
-    <div className="mt-12 md:mt-32">
-      <div className="text-purple-700 text-center text-2xl md:text-3xl ">Our Partners and Sponsors</div>
-      <div className="bg-purple-200 mt-4">
-      <Ticker>
-        {({ index }) => (
-          <>
-            <div>
-                <span>jeie</span>
-                <span>adam</span>
-                
-            </div>
-          </>
-        )}
-      </Ticker>
+    <div className="mt-12 md:mt-32 p-4 ">
+      <div className="text-purple-700 text-center text-2xl md:text-3xl ">
+        Our Partners and Sponsors
       </div>
-      
+      <div className="text-center text-gray-400">
+        Trusted by <span className="text-purple-700 font-bold">100+ </span>
+        Schools and Sponsored by{" "}
+        <span className="text-purple-700 font-bold">500+</span> Amazing
+        Individuals and Organizations
+      </div>
+      <div className="mt-4  container">
+        <Ticker>
+          {({ index }) => (
+            <>
+              <div className="mx-6">
+                <Image
+                  src="/google.svg"
+                  alt="Google"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            </>
+          )}
+        </Ticker>
+      </div>
     </div>
   );
 }
