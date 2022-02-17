6d1d5f036f05a8ada48660cee160b701145f7ade
sammy-code98 <sammysmart98@yahoo.com>
2022-02-17 22:03:43 +0100

    fix: mission statement content

--- components/Mission/Mission.jsx
+++ components/Mission/Mission.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { FaHandshake, FaRocket, FaRobot , FaPi} from "react-icons/fa";
+import {
+  FaHandshake,
+  FaRocket,
+  FaRobot,
+  FaSquareRootAlt,
+} from "react-icons/fa";
 
 function Mission() {
   return (
@@ -13,18 +18,42 @@ function Mission() {
         voluptatum laboriosam assumenda impedit amet, blanditiis totam
         voluptates, excepturi neque beatae reiciendis!
       </div>
-      <div className="grid grid-cols-4 gap-6 justify-items-center mt-12">
+      <div className="grid grid-cols-4 gap-8 justify-items-center mt-12 px-24">
         <div>
-          <FaHandshake style={{ fontSize: "4em" }} />
+          <FaHandshake style={{ fontSize: "3em" }} />
+          <div className="text-base text-gray-700 mt-2 ">Lorem</div>
+          <p className="text-sm text-gray-500">
+            These utilities are really just a shortcut for adding margin to
+            all-but-the-first-item in a group, and aren’t designed to handle
+            complex cases like grids, layouts that wrap, or situations where the
+          </p>
         </div>
         <div>
-            <FaRocket  style={{ fontSize:"4em"}}/>
+          <FaRocket style={{ fontSize: "3em" }} />
+          <div className="text-base text-gray-700 mt-2 ">Lorem</div>
+          <p className="text-sm text-gray-500">
+            These utilities are really just a shortcut for adding margin to
+            all-but-the-first-item in a group, and aren’t designed to handle
+            complex cases like grids, layouts that wrap, or situations where the
+          </p>
         </div>
         <div>
-            <FaRobot style={{ fontSize:"4em"}}/>
+          <FaRobot style={{ fontSize: "3em" }} />
+          <div className="text-base text-gray-700  mt-2">Lorem</div>
+          <p className="text-sm text-gray-500">
+            These utilities are really just a shortcut for adding margin to
+            all-but-the-first-item in a group, and aren’t designed to handle
+            complex cases like grids, layouts that wrap, or situations where the
+          </p>
         </div>
         <div>
-            <FaPi style={{ fontSize:"4em"}}/> 
+          <FaSquareRootAlt style={{ fontSize: "3em" }} />
+          <div className="text-base text-gray-700 mt-2">Lorem</div>
+          <p className="text-sm text-gray-500">
+            These utilities are really just a shortcut for adding margin to
+            all-but-the-first-item in a group, and aren’t designed to handle
+            complex cases like grids, layouts that wrap, or situations where the
+          </p>
         </div>
       </div>
     </section>
