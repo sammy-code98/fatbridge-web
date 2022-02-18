0341a989b7c1b974bd28d70704ea066ca534beb6
sammy-code98 <sammysmart98@yahoo.com>
2022-02-18 21:55:16 +0100

    feat:redesign faq page - refacotor code - make new ui design changes

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -1,174 +1,114 @@
 import React from "react";
+import { FaEyes } from "react-icons/fa";
 
 function Faq() {
   return (
-    <section className="md:grid md:grid-cols-2 md:gap-6 mt-24 md:mt-32 md:justify-items-center md:px-12">
+    <>
+      <section className="md:grid md:grid-cols-2 md:gap-6 mt-24 md:mt-32 md:justify-items-center md:px-12">
+        <div className="mt-12">
+          <p className="text-center text-2xl md:text-4xl p-2 text-gray-500">
+            You have questions?
+          </p>
+          <p className="text-center text-2xl md:text-4xl p-2 text-gray-500">
+            We have answers
+          </p>
+        </div>
+        <div className="mt-4">
+          <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
+            <input type="checkbox" />
+            <div className="collapse-title text-lg text-white font-medium">
+              Lorem ipsum d
+            </div>
+            <div className="collapse-content text-white">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas quo assumenda consequatur minima dolores quasi soluta
+                adipisci explicabo reprehenderit, delectus at blanditiis sunt
+                doloribus dolore! Rerum assumenda placeat est rem.
+              </p>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
+            <input type="checkbox" />
+            <div className="collapse-title text-lg text-white font-medium">
+              Lorem ipsum d
+            </div>
+            <div className="collapse-content text-white">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas quo assumenda consequatur minima dolores quasi soluta
+                adipisci explicabo reprehenderit, delectus at blanditiis sunt
+                doloribus dolore! Rerum assumenda placeat est rem.
+              </p>
+            </div>
+          </div>
+          {/* 3 */}
+          <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
+            <input type="checkbox" />
+            <div className="collapse-title text-lg text-white font-medium">
+              Lorem ipsum d
+            </div>
+            <div className="collapse-content text-white">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas quo assumenda consequatur minima dolores quasi soluta
+                adipisci explicabo reprehenderit, delectus at blanditiis sunt
+                doloribus dolore! Rerum assumenda placeat est rem.
+              </p>
+            </div>
+          </div>
+          {/* 4 */}
+          <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
+            <input type="checkbox" />
+            <div className="collapse-title text-lg text-white font-medium">
+              Lorem ipsum d
+            </div>
+            <div className="collapse-content text-white">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas quo assumenda consequatur minima dolores quasi soluta
+                adipisci explicabo reprehenderit, delectus at blanditiis sunt
+                doloribus dolore! Rerum assumenda placeat est rem.
+              </p>
+            </div>
+          </div>
+          {/* 5 */}
+          <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
+            <input type="checkbox" />
+            <div className="collapse-title text-lg text-white font-medium">
+              Lorem ipsum d
+            </div>
+            <div className="collapse-content text-white">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas quo assumenda consequatur minima dolores quasi soluta
+                adipisci explicabo reprehenderit, delectus at blanditiis sunt
+                doloribus dolore! Rerum assumenda placeat est rem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
       <div className="mt-12">
-        <p className="text-center text-2xl md:text-4xl p-2 text-gray-500">You have questions?</p>
-        <p className="text-center text-2xl md:text-4xl p-2 text-gray-500">We have answers</p>
+        <div className="text-center text-2xl md:text-4xl text-gray-500">
+          Still Need Help? <span>👀</span>
+        </div>
+        <div className="text-center text-lg text-gray-400">
+          Our team is happy to answer your questions. Contact us and we&apos;ll
+          be in touch as soon as possible.
+        </div>
+        <div className="flex flex-row  justify-center mt-4">
+        <button
+          type="submit"
+          className="p-4 text-center py-3 rounded-full bg-purple-700 text-white shadow-md focus:outline-none my-1"
+        >
+        Contact Support
+        </button>
+        </div>
+       
       </div>
-      <div className="mt-6">
-        <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-          <input type="checkbox" />
-          <div className="collapse-title text-lg text-white font-medium">
-            Lorem ipsum d
-          </div>
-          <div className="collapse-content text-white">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              quo assumenda consequatur minima dolores quasi soluta adipisci
-              explicabo reprehenderit, delectus at blanditiis sunt doloribus
-              dolore! Rerum assumenda placeat est rem.
-            </p>
-          </div>
-        </div>
-        {/* 2 */}
-        <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-          <input type="checkbox" />
-          <div className="collapse-title text-lg text-white font-medium">
-            Lorem ipsum d
-          </div>
-          <div className="collapse-content text-white">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              quo assumenda consequatur minima dolores quasi soluta adipisci
-              explicabo reprehenderit, delectus at blanditiis sunt doloribus
-              dolore! Rerum assumenda placeat est rem.
-            </p>
-          </div>
-        </div>
-        {/* 3 */}
-        <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-          <input type="checkbox" />
-          <div className="collapse-title text-lg text-white font-medium">
-            Lorem ipsum d
-          </div>
-          <div className="collapse-content text-white">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              quo assumenda consequatur minima dolores quasi soluta adipisci
-              explicabo reprehenderit, delectus at blanditiis sunt doloribus
-              dolore! Rerum assumenda placeat est rem.
-            </p>
-          </div>
-        </div>
-        {/* 4 */}
-        <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-          <input type="checkbox" />
-          <div className="collapse-title text-lg text-white font-medium">
-            Lorem ipsum d
-          </div>
-          <div className="collapse-content text-white">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              quo assumenda consequatur minima dolores quasi soluta adipisci
-              explicabo reprehenderit, delectus at blanditiis sunt doloribus
-              dolore! Rerum assumenda placeat est rem.
-            </p>
-          </div>
-        </div>
-        {/* 5 */}
-        <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-          <input type="checkbox" />
-          <div className="collapse-title text-lg text-white font-medium">
-            Lorem ipsum d
-          </div>
-          <div className="collapse-content text-white">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              quo assumenda consequatur minima dolores quasi soluta adipisci
-              explicabo reprehenderit, delectus at blanditiis sunt doloribus
-              dolore! Rerum assumenda placeat est rem.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-    // <div className="mt-24 md:mt-32">
-    //   <div className="text-2xl md:text-4xl text-center text-gray-500">
-    //     Frequently <span className="text-purple-700">Asked </span>Questions
-    //   </div>
-    //   <div className="text-center text-md text-gray-400">
-    //     Get answers to similar questions you might have about the{" "}
-    //     <span className="text-purple-700">FATBRIDGE STEM + C </span>program
-    //   </div>
-    //   <div className="flex flex-col  mt-4 md:px-32 pt-4">
-    //     <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-    //       <input type="checkbox" />
-    //       <div className="collapse-title text-lg text-white font-medium">
-    //         Lorem ipsum d
-    //       </div>
-    //       <div className="collapse-content text-white">
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-    //           quo assumenda consequatur minima dolores quasi soluta adipisci
-    //           explicabo reprehenderit, delectus at blanditiis sunt doloribus
-    //           dolore! Rerum assumenda placeat est rem.
-    //         </p>
-    //       </div>
-    //     </div>
-    //     {/* 2 */}
-    //     <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-    //       <input type="checkbox" />
-    //       <div className="collapse-title text-lg text-white font-medium">
-    //         Lorem ipsum d
-    //       </div>
-    //       <div className="collapse-content text-white">
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-    //           quo assumenda consequatur minima dolores quasi soluta adipisci
-    //           explicabo reprehenderit, delectus at blanditiis sunt doloribus
-    //           dolore! Rerum assumenda placeat est rem.
-    //         </p>
-    //       </div>
-    //     </div>
-    //     {/* 3 */}
-    //     <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-    //       <input type="checkbox" />
-    //       <div className="collapse-title text-lg text-white font-medium">
-    //         Lorem ipsum d
-    //       </div>
-    //       <div className="collapse-content text-white">
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-    //           quo assumenda consequatur minima dolores quasi soluta adipisci
-    //           explicabo reprehenderit, delectus at blanditiis sunt doloribus
-    //           dolore! Rerum assumenda placeat est rem.
-    //         </p>
-    //       </div>
-    //     </div>
-    //     {/* 4 */}
-    //     <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-    //       <input type="checkbox" />
-    //       <div className="collapse-title text-lg text-white font-medium">
-    //         Lorem ipsum d
-    //       </div>
-    //       <div className="collapse-content text-white">
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-    //           quo assumenda consequatur minima dolores quasi soluta adipisci
-    //           explicabo reprehenderit, delectus at blanditiis sunt doloribus
-    //           dolore! Rerum assumenda placeat est rem.
-    //         </p>
-    //       </div>
-    //     </div>
-    //     {/* 5 */}
-    //     <div className="collapse w-full bg-purple-500 rounded-box shadow-sm mb-4 collapse-arrow">
-    //       <input type="checkbox" />
-    //       <div className="collapse-title text-lg text-white font-medium">
-    //         Lorem ipsum d
-    //       </div>
-    //       <div className="collapse-content text-white">
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-    //           quo assumenda consequatur minima dolores quasi soluta adipisci
-    //           explicabo reprehenderit, delectus at blanditiis sunt doloribus
-    //           dolore! Rerum assumenda placeat est rem.
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    </>
   );
 }
 
