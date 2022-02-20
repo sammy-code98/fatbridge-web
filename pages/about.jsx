1c710f50cb3740587ae2823f2f37c1e8fb1ca9aa
sammy-code98 <sammysmart98@yahoo.com>
2022-02-20 21:08:54 +0100

    updating about section

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
 function about() {
   return (
     <section className="pt-24 container">
-      <div className="text-red-400 ">
-        helleo
-      
+      <div className="md:grid md:grid-cols-2 md:gap-6 md:justify-items-center">
+        <div className="md:mt-12 md:pl-12">
+          <div className="text-4xl text-gray-500 text-center">Get to Know Us</div>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Distinctio, recusandae ipsum at numquam, quo explicabo atque sint
+            nobis.
+          </div>
+        </div>
+        <div>
+          <Image src="/about.svg" width={500} height={300} alt="About" />
+        </div>
       </div>
     </section>
   );
