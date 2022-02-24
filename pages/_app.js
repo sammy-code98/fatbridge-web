cca0f0d1de5ec046a9c9ccb74ac8e64eb59a58da
sammy-code98 <sammysmart98@yahoo.com>
2022-02-24 19:58:00 +0100

    clean up code

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
 
-
-
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,21 @@
-
 import Banner from "../components/Banner";
-import Vision from "../components/Vision/Vision"
-import Mission from "../components/Mission/Mission"
-import Partners from "../components/Partners"
-import Testimony from "../components/Testimony/Testimony"
-import Faq from "../components/Faq"
+import Vision from "../components/Vision/Vision";
+import Mission from "../components/Mission/Mission";
+import Partners from "../components/Partners";
+import Testimony from "../components/Testimony/Testimony";
+import Faq from "../components/Faq";
 
-
- export default function Home() {
+export default function Home() {
   return (
-      <main id="home"  className="container" >
-        <Banner  />
-        <div>
-          <Vision/>
-          <Mission/>
-          <Partners/>
-          <Testimony/>
-          <Faq/>
-        </div>
-      </main>
+    <main id="home" className="container">
+      <Banner />
+      <div>
+        <Vision />
+        <Mission />
+        <Partners />
+        <Testimony />
+        <Faq />
+      </div>
+    </main>
   );
 }
-// Home.layout = "L1"
-
