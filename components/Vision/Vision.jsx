ee3ab4c2d00bad5b6c15942fb90e4629cc9ac117
sammy-code98 <sammysmart98@yahoo.com>
2022-02-28 20:29:00 +0100

    fix: add padding bottom to vision cards

--- components/Vision/Vision.jsx
+++ components/Vision/Vision.jsx
@@ -8,8 +8,8 @@ import {
 
 function Vision() {
   return (
-    <div className="md:grid  md:grid-cols-4 md:gap-6 md:justify-items-center p-4 md:px-24  mt-24 sm:grid sm:grid-cols-2 sm:gap-8">
-      <div className="bg-purple-200 w-full rounded-md shadow-md mt-24">
+    <div className="md:grid  md:grid-cols-4 md:gap-6 md:justify-items-center p-4 md:px-24  mt-12 sm:grid sm:grid-cols-2 sm:gap-8">
+      <div className="bg-purple-200 w-full rounded-md shadow-md mt-24 pb-4">
         <div className="flex mx-auto -translate-y-12 rounded-full  shadow-2xl h-20 w-20 text-gray-700  bg-white">
           <FaGraduationCap
             className="flex mx-auto mt-4  text-gray-500 "
@@ -24,7 +24,7 @@ function Vision() {
         </div>
       </div>
       {/* 2nd */}
-      <div className="bg-amber-200 w-full rounded-md shadow-md mt-24">
+      <div className="bg-amber-200 w-full rounded-md shadow-md mt-24 pb-4">
         <div className="flex mx-auto -translate-y-12 rounded-full text-center shadow-2xl h-20 w-20 text-gray-700  bg-white">
           <FaBrain
             className="flex mx-auto mt-4  text-gray-500 "
@@ -39,7 +39,7 @@ function Vision() {
         </div>
       </div>
       {/* 3rd */}
-      <div className="bg-green-200 w-full rounded-md shadow-md mt-24">
+      <div className="bg-green-200 w-full rounded-md shadow-md mt-24 pb-4">
         <div className="flex mx-auto -translate-y-12 rounded-full text-center shadow-2xl h-20 w-20 text-gray-700  bg-white">
           <FaFlask
             className="flex mx-auto mt-4  text-gray-500 "
@@ -54,7 +54,7 @@ function Vision() {
         </div>
       </div>
       {/* 4th */}
-      <div className="bg-yellow-200 w-full rounded-md shadow-md mt-24">
+      <div className="bg-yellow-200 w-full rounded-md shadow-md mt-24 pb-4">
         <div className="flex mx-auto -translate-y-12 rounded-full text-center shadow-2xl h-20 w-20 text-gray-700  bg-white">
           <FaCalculator
             className="flex mx-auto mt-4  text-gray-500 "
