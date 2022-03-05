ffa6eab4540ec4184440392d19c1b0654c7e7f5d
sammy-code98 <sammysmart98@yahoo.com>
2022-03-05 23:53:48 +0100

    clean up

--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -1,14 +1,1 @@
-import React from "react";
 
-
-import "@glidejs/glide/src/assets/sass/glide.core";
-
-// Optional Theme Stylesheet
-import "@glidejs/glide/src/assets/sass/glide.theme";
-import "./styles.css";
-import Glide from "@glidejs/glide";  
-function Team() {
-  return <div>Team</div>;
-}
-
-export default Team;
