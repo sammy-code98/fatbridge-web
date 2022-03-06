215427bb67886b5e296819fee7abc2f1890d8f70
sammy-code98 <sammysmart98@yahoo.com>
2022-03-06 21:39:41 +0100

    feat: slider for team

--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -1,7 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
 
 export default function Team() {
   return (
-    <div>Team</div>
+    <div className="container px-12 mt-12 pb-24">
+        <Splide options={{ 
+            rewind: true,
+            speed: 400,
+            rewindSpeed:1000,
+            width:'100%',
+            height:'15rem',
+            start:3,
+            perPage:3,
+            perMove:1,
+            pagination:false,
+            drag: true,
+            trimSpace: 'move',
+            gap:"10px"
+
+        }}>
+  <SplideSlide>
+    <img src="/me.jpeg" alt="Image 1"/>
+  </SplideSlide>
+  <SplideSlide>
+    <img src="/new.png" alt="Image 2"/>
+  </SplideSlide>
+  <SplideSlide>
+    <img src="/new.png" alt="Image 2"/>
+  </SplideSlide><SplideSlide>
+    <img src="/new.png" alt="Image 2"/>
+  </SplideSlide><SplideSlide>
+    <img src="/new.png" alt="Image 2"/>
+  </SplideSlide><SplideSlide>
+    <img src="/new.png" alt="Image 2"/>
+  </SplideSlide>
+  <SplideSlide>
+    <img src="/new.png" alt="Image 2"/>
+  </SplideSlide><SplideSlide>
+    <img src="/new.png" alt="Image 2"/>
+  </SplideSlide>
+</Splide>
+    </div>
   )
 }
