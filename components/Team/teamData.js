9c763f8c856ec7491925e221af01ba3dce5b679d
sammy-code98 <sammysmart98@yahoo.com>
2022-03-08 23:46:49 +0100

    updating team section

---
+++ components/Team/teamData.js
@@ -0,0 +1,18 @@
+export  default teamData = [
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+
+    
+
+]
