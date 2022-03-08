5c9bd8130d48324ac20b7d866452b474f08cef61
sammy-code98 <sammysmart98@yahoo.com>
2022-03-08 23:52:29 +0100

    update

--- components/Team/teamData.js
+++ components/Team/teamData.js
@@ -1,17 +1,17 @@
-export  default teamData = [
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
-    {name: "Mrs. Mary Ejiogu", img:"/public/team/mary.jpg", role:"Tutor"},
+export  const teamData = [
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
+    {name: "Mrs. Mary Ejiogu", img:"/team/mary.jpg", role:"Tutor"},
 
     
 
